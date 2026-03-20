71504c7bf3ea452a93a7187404d86b53319dcfb8
dveininnovationsprojects-cpu <dveininnovations.project@gmail.com>
2026-03-20 12:10:45 +0530

    removed tensorflow

--- test2.py
+++ test2.py
@@ -2,11 +2,10 @@ import cv2
 from ultralytics import YOLO
 import numpy as np
 def predict(img):
-    model = YOLO(r"D:\SHIVESANGKER-Water Mark Project\SHIVESANGKER-Water Mark Project\runs\classify\train11\weights\best.pt")
+    model = YOLO("weights\best.pt")
     results = model(img, conf=0.4)
     return results[0].probs.data
-def predict(img):
-    return [0.5, 0.5]
+
 #f=open(r"c:\Users\lenovo\OneDrive\Pictures\banker 2.jpg",'rb')
 #nparr = np.frombuffer(f.read(), np.uint8)
 #img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
